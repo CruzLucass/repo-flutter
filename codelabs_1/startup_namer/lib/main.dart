d7abdad7bf8d0d3ffd7bec0ea73743a43d1db7d1
LucasCruz <lucaas-cruuz@live.com>
2022-03-08 19:06:44 -0300

    Codelabs_1 e codelabs_2

--- codelabs_1/startup_namer/lib/main.dart
+++ codelabs_1/startup_namer/lib/main.dart
@@ -34,6 +34,7 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
